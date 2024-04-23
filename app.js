c53413626c2e5c63cae52ee1debf04dd616449c0
Khaled Bin Monowar Hossain <111820117+khbmh@users.noreply.github.com>
2024-04-23 10:39:07 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+function show(btn, item){btn.addEventListener(`click`,()=>{
+  item.style.display = `flex`
+  // alert(`hello`)
+})}
+function hide(btn, item){btn.addEventListener(`click`,()=>{
+  item.style.display = `none`
+  // alert(`hello`)
+})}
+
+
+show(document.querySelector(`#sl6op`), document.querySelector(`#sl6`))
+hide(document.querySelector(`#sl6cl`), document.querySelector(`#sl6`))
+show(document.querySelector(`#os6op`), document.querySelector(`#os6`))
+hide(document.querySelector(`#os6cl`), document.querySelector(`#os6`))
+
+
+
+show(document.querySelector(`#sl7op`), document.querySelector(`#sl7`))
+hide(document.querySelector(`#sl7cl`), document.querySelector(`#sl7`))
+show(document.querySelector(`#os7op`), document.querySelector(`#os7`))
+hide(document.querySelector(`#os7cl`), document.querySelector(`#os7`))
+
+
+
+show(document.querySelector(`#sl8op`), document.querySelector(`#sl8`))
+hide(document.querySelector(`#sl8cl`), document.querySelector(`#sl8`))
+show(document.querySelector(`#os8op`), document.querySelector(`#os8`))
+hide(document.querySelector(`#os8cl`), document.querySelector(`#os8`))
+
+
+
+show(document.querySelector(`#sl9op`), document.querySelector(`#sl9`))
+hide(document.querySelector(`#sl9cl`), document.querySelector(`#sl9`))
+show(document.querySelector(`#os9op`), document.querySelector(`#os9`))
+hide(document.querySelector(`#os9cl`), document.querySelector(`#os9`))
+
+
+show(document.querySelector(`#sl10op`), document.querySelector(`#sl10`))
+hide(document.querySelector(`#sl10cl`), document.querySelector(`#sl10`))
+show(document.querySelector(`#os10op`), document.querySelector(`#os10`))
+hide(document.querySelector(`#os10cl`), document.querySelector(`#os10`))
+
+
+
+
+
+
+
+
+
+
